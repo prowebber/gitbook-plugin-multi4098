e9c5dd6b3a2891e41a762aa5986950ba4d18a00d
Steven Holdaway <webpurellc@gmail.com>
2019-02-23 10:47:10 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = {
+    blocks: {
+        tag1:{
+            process: function (block) {
+                // Get arguments passed via the {}
+                let title = block.kwargs.title || '';
+                let open = block.kwargs.loadopen || 'no';
+
+                // Generate the final HTML
+                function generateHtml(title, data, open){
+                    open = (open == 'yes') ? 'open': '';
+
+                    let  html = "<details " + open + ">";
+                    html += "<summary>" + title + "</summary>";
+                    html += data;
+                    html += "</details>";
+
+                    return html;
+                }
+
+                // Convert the markdown to HTML
+                let config = this.renderBlock('markdown', block.body).then(function(data){
+                    return generateHtml(title, data, open);
+                });
+
+                return config;
+            }
+        }
+    }
+};
